24ecae01fa8b717c32f41651d38450b2575ee8bb
Osbaldo <ocruz1998.oc@gmail.com>
2024-03-16 15:57:14 -0700

    added images

--- index.js
+++ index.js
@@ -11,3 +11,42 @@ pineTree.style.position = 'fixed'
 pineTree.style.left = '450px'
 pineTree.style.bottom = '200px'
 document.body.append(pineTree)
+
+let tree = document.createElement('img')
+tree.src = 'assets/tree.png'
+tree.style.position = 'fixed'
+tree.style.left = '200px'
+tree.style.bottom = '300px'
+document.body.append(tree)
+
+let pillar = document.createElement('img')
+pillar.src = 'assets/pillar.png'
+pillar.style.position = 'fixed'
+pillar.style.left = '350px'
+pillar.style.bottom = '100px'
+document.body.append(pillar)
+
+let crate = document.createElement('img')
+crate.src = 'assets/crate.png'
+crate.style.position = 'fixed'
+crate.style.left = '150px'
+crate.style.bottom = '200px'
+document.body.append(crate)
+
+let well = document.createElement('img')
+well.src = 'assets/well.png'
+well.style.position = 'fixed'
+well.style.left = '500px'
+well.style.bottom = '425px'
+document.body.append(well)
+
+let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
